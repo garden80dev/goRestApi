c3667bc973511282cc91085b75f9abe28b4dae34
garden <garden80@gmail.com>
2022-12-01 16:46:45 +0100

    introduced db code

--- main.go
+++ main.go
@@ -22,20 +22,22 @@ type measurements []measurement
 
 var remark = measurements{
 	{
+		Id: "0001"
 		Timestamp: "2022-10-04T15:15:47.000Z",
 		Position:  "position1",
 		Temp:      "50",
 		Omega:     "ooo",
 		Speed:     "sss",
-		Car_id:    "0001",
+		Car_id:    "Ferrari",
 	},
 	{
+		Id: "0002"
 		Timestamp: "2022-11-04T15:15:47.000Z",
 		Position:  "position2",
 		Temp:      "80",
 		Omega:     "ooo2",
 		Speed:     "sss2",
-		Car_id:    "0002",
+		Car_id:    "Maserati",
 	},
 }
 
@@ -45,6 +47,22 @@ func homeLink(w http.ResponseWriter, r *http.Request) {
 
 func getAllMeasurements(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(remark)
+}
+
+func openDb() {
+	database, _ := sql.Open("sqlite3", "./ranks.db")
+    //statement, _ := database.Prepare("CREATE TABLE IF NOT EXISTS AllRanks (id INTEGER PRIMARY KEY, timestamp TEXT, press INTEGER, )")
+    //statement.Exec()
+    //statement, _ = database.Prepare("INSERT INTO AllRanks (timestamp, press) VALUES (?, ?)")
+    //statement.Exec("Nic", "Raboy")
+    rows, _ := database.Query("SELECT id, timestamp, press FROM AllRanks")
+    var id int
+    var timestamp string
+    var press int
+    for rows.Next() {
+        rows.Scan(&id, &timestamp, &press)
+        fmt.Println(strconv.Itoa(id) + ": " + timestamp + " " + press)
+    }
 }
 
 func main() {
